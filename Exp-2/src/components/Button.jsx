d975c899a2d5a43bb18d70cf7489e950f8dc6d9f
Himesh <himeshfegade09@gmail.com>
2026-01-15 11:00:34 +0530

    updated button.jsx

--- Exp-2/src/components/Button.jsx
+++ Exp-2/src/components/Button.jsx
@@ -1,26 +1,27 @@
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Rating from '@mui/material/Rating';
+import Slider from '@mui/material/Slider';
+import ToggleButton from '@mui/material/ToggleButton';
+import { lazy } from 'react';
 
 export default function ButtonBasic() {
   return (
     <>
     <Button size="small" variant='outlined'>Small</Button>
-    <Button size="medium" variant='outlined'>Medium</Button>
-    <Button size="large" variant='outlined'>Large</Button>
     <br/>
-    <br/>
-    <Button size='small' variant='contained'>Small</Button>
     <Button size="medium" variant='contained'>Medium</Button>
-    <Button size="large" variant='contained'>Large</Button>
     <br/>
-    <br/>
-    <Button size='small' variant='text'>Small</Button>
-    <Button size="medium" variant='text'>Medium</Button>
     <Button size="large" variant='text'>Large</Button>
     <br/>
-    <br/>
     <TextField label="Small" size="small" variant="outlined"/>
-    <TextField label="Medium" size="medium" variant="outlined" />
+    <br/>
+    <Rating name="size-medium" defaultValue={2} size="medium" />
+    <br/>
+    <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
+    <br/>
+    <ToggleButton value="web">Web</ToggleButton>
+    <br/>
     </>
   ) 
 }
